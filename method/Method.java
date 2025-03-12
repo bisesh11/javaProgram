03133f167c5d335e02d71c2d886d81d6152c84c6
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:52:07 +0530

    method

--- method/Method.java
+++ method/Method.java
@@ -3,10 +3,13 @@ public class Method {
     /*
      * method file
      * using variable
+     * addition of two 
      */
      public static void main(String[] args) {
-        int x=10; //this is int value
-        System.out.println("hello");
-        System.out.println(x);
+        int x=10; //this is int x value
+        int y=55; //this is int y value
+        int z=x+y; // result 
+        System.out.println("Addition of 10 & 55: "+x);
     }
 }
+
