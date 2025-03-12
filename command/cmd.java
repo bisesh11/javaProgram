8800378f36a06fa9311c0e777fa623cac22c903f
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:32:17 +0530

    ok

--- command/cmd.java
+++ command/cmd.java
@@ -8,9 +8,9 @@ public class cmd {
     public void add(int x,int y) {
         int z=x+y;
         System.out.println(z);
-        {
+    }
+    public static void main(String[] args) {
         cmd obj=new cmd();
         obj.add(10,20);
     }
-}
 }
