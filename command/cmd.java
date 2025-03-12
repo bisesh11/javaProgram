6f630aa248b3476ab40289bc82e64d1dbc46e90e
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:20:49 +0530

    ok

--- command/cmd.java
+++ command/cmd.java
@@ -1,5 +1,10 @@
 package command;
 public class cmd {
+    /*
+    1 cmd file
+    2 using variable 
+      
+     */
     public void add(int x,int y) {
         int z=x+y;
         System.out.println(z);
