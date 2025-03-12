db744989472da6e249a6724361f76c05db665fdd
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:42:57 +0530

    method

---
+++ method/Method.java
@@ -0,0 +1,12 @@
+package method;
+public class Method {
+    /*
+     * method file
+     * using variable
+     */
+     public static void main(String[] args) {
+        int x=10; //this is int value
+        System.out.println("hello");
+        System.out.println(x);
+    }
+}
