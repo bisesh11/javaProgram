291664e3db9060468428b785e95238a5178fc875
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:21:59 +0530

    ok

--- variable/Demo.java
+++ variable/Demo.java
@@ -3,6 +3,7 @@ public class Demo {
     public static void main(String[] args) {
         int x=10; //this is int value
         System.out.println("hello");
+        System.out.println(x);
     }
 
 }
