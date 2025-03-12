24ef373e381d338f9864d64849c2c991a898ccc2
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 14:16:21 +0530

    ok

--- command/cmd.java
+++ command/cmd.java
@@ -1,5 +1,11 @@
 package command;
-
 public class cmd {
-    
+    public void add(int x,int y) {
+        int z=x+y;
+        System.out.println(z);
+        {
+        cmd obj=new cmd();
+        obj.add(10,20);
+    }
+}
 }
--- variable/Demo.java
+++ variable/Demo.java
@@ -1,6 +1,7 @@
 package variable;
 public class Demo {
     public static void main(String[] args) {
+        int x=10; //this is int value
         System.out.println("hello");
     }
 
