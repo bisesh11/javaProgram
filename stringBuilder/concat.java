956b023424d4097c16d1a3e69657628e7c69753a
bisesh11 <biseshranjanl@gmail.com>
2025-03-12 15:52:48 +0530

    return_type

---
+++ stringBuilder/concat.java
@@ -0,0 +1,12 @@
+package stringBuilder;
+public class concat {
+    
+    public void conact(String x,String y) {
+        String z=x+y;
+        System.out.println(z);
+    }
+    public static void main(String[] args) {
+        concat obj=new concat();
+        obj.concat(Niraj ,Kumar);
+    }
+}
