1f0c31f76ddf648e6c5170f48fb68ad37fad8610
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 11:02:42 +0530

    3

---
+++ ConditionalOperator/cond.java
@@ -0,0 +1,12 @@
+package ConditionalOperator;
+public class cond {
+        public static void main(String args[]){
+            System.out.println(10<2);
+            System.out.println(10>2);
+            System.out.println(10<=2);
+            System.out.println(10>=2);
+            System.out.println(10==11);
+            System.out.println(10!=2);
+        }
+}
+// use <,>,<=,>=,==,!=
---
+++ ConditionalStatement/state.java
@@ -0,0 +1,11 @@
+package ConditionalStatement;
+public class state {
+    public static void main(String[] args) {
+        if(10>9){
+            System.out.println("True");
+        }
+        if(12>90){
+            System.out.println("false");
+        }
+    }
+}
