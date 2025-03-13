49427af7ad9572e82f8308573e2c2c058ec8b224
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 11:45:46 +0530

    gender

---
+++ genderFinder/gender.java
@@ -0,0 +1,14 @@
+package genderFinder;
+
+public class gender {
+    
+    public static void main(String args[]){
+        String a= "M";
+        if(a=="M"){
+            System.out.println("Male");
+        }
+        else{
+            System.out.println("female");
+        }
+    }
+}
