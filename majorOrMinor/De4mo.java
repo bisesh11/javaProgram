6f62fd8fe5a40503b057a563306b412d591e774c
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 11:19:02 +0530

    major or minor

---
+++ majorOrMinor/De4mo.java
@@ -0,0 +1,13 @@
+package majorOrMinor;
+
+public class De4mo {
+    public static void main(String args[]){
+        int a=19;
+        if(a<=19){
+            System.out.println("Minor");
+        }
+        else{
+            System.out.println("Major");
+        }
+    }
+}
