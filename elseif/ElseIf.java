c3bc1aa9011649de1c3922e1e627e2592b6721db
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 12:00:39 +0530

    nn

---
+++ elseif/ElseIf.java
@@ -0,0 +1,16 @@
+package elseif;
+
+public class ElseIf {
+    public static void main(String args[]){
+        int a=1;
+        if(a<0){
+            System.out.println("negative");
+        }
+        else if(a>=1){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("the no is 0");
+        }
+    }
+}
---
+++ positiveNegative/PosNeg.java
@@ -0,0 +1,14 @@
+package positiveNegative;
+
+public class PosNeg {
+    public static void main(String args[]){
+        int a=1;
+        if(a<0){
+            System.out.println("negative");
+        }       
+        else{
+            System.out.println("positive");
+        }
+    }
+    
+}
