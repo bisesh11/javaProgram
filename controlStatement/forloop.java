b734f9afd0df7850ec9fd61b76ceb1ab1bfdd1d5
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 14:07:11 +0530

    -_-

---
+++ controlStatement/forloop.java
@@ -0,0 +1,13 @@
+package controlStatement;
+
+public class forloop{
+    public static void main(String args[]){
+        /* 
+        syntax
+        for(initial part;conditional part;unary perator)
+        */
+        for(int i=0;i<9;i++)
+        System.out.println(i);
+    }
+    
+}
--- multiDimensionalArray/arr.java
+++ multiDimensionalArray/arr.java
@@ -3,6 +3,7 @@ package multiDimensionalArray;
 public class arr {
     public static void main(String args[]){
         int a[][]={{2,3,4},{2,5,4}};
-        System.out.println(a[0][1]);
+        System.out.println(a[0][1]);   
+        System.out.println(a[1][2]);
     }
 }
