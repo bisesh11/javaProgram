4aa4902b064ce98d324559e0a52d39e14b56c6a8
bisesh11 <biseshranjanl@gmail.com>
2025-03-13 15:09:22 +0530

    >_<

--- leap/leapyear.java
+++ leap/leapyear.java
@@ -4,10 +4,10 @@ public class leapyear {
     public static void main(String[] args) {
         for(int i=2020;i<2031;i++){
         if((i % 4==0 && i % 100 !=0) || (i % 400 == 0)){
-            System.out.println(i+"leap year");
+            System.out.println(i+"  leap year");
         }
         else{
-            System.out.println(i +"not");
+            System.out.println(i +"  not");
         }
     }
     }
---
+++ nextint/scanner.java
@@ -0,0 +1,12 @@
+package nextint;
+import java.util.*;
+public class scanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your age:");
+        int age=input.nextInt();
+        System.out.println("Your age:" +age);
+
+    }
+    
+}
