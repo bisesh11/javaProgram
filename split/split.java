92df131cbeb05bafeb99f68c9f5a7ca7ebf8f325
bisesh11 <biseshranjanl@gmail.com>
2025-03-14 11:49:31 +0530

    >^<

---
+++ split/split.java
@@ -0,0 +1,10 @@
+package split;
+
+public class split {
+    public static void main(String args[]){
+        String name="Wel,come";
+        String[] names=name.split(",");
+        System.out.println(names[0]);
+    }
+    
+}
