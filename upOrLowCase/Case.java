c9cae23b77757d96ac32ff275520bb4513502a06
bisesh11 <biseshranjanl@gmail.com>
2025-03-14 12:03:17 +0530

    >-< case

---
+++ upOrLowCase/Case.java
@@ -0,0 +1,9 @@
+package upOrLowCase;
+
+public class Case {
+    public static void main(String[] args) {
+    String name="Hello jack ";
+    System.out.println(name.toUpperCase());
+    System.out.println(name.toLowerCase());
+}
+}
