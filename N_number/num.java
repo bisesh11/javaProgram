85d800f3b454dd963bdef423ecbe9f9b66be4ad4
bisesh11 <biseshranjanl@gmail.com>
2025-03-14 10:32:30 +0530

    n number

---
+++ N_number/num.java
@@ -0,0 +1,18 @@
+package N_number;
+
+import java.util.Scanner;
+
+public class num {
+    public static void main(String args[]){
+        Scanner i = new Scanner(System.in);2
+        System.out.println("enter any no.:");
+        int in=i.nextInt();
+        Scanner j = new Scanner(System.in);
+        System.out.println("enter last no.:");
+        int jn=j.nextInt();
+        while(in<=jn){
+        System.out.println("no: " +in);
+            in++;
+        }
+    }
+}
---
+++ table/tab.java
@@ -0,0 +1,12 @@
+package table;
+
+public class tab {
+    public static void main(String args[]){
+        int i=1;
+        int j=7;
+        while(i<=10){
+            System.out.println( "7 * "+i+ " = "+j*i);
+            i++;
+        }
+    }
+}
