71ff088cb5aa188e227a216ec37fffe52865a159
bisesh11 <biseshranjanl@gmail.com>
2025-03-15 11:42:37 +0530

    ::

---
+++ constructor/cons.java
@@ -0,0 +1,12 @@
+package constructor;
+ 
+ public class cons{
+ 
+     public cons() {
+         System.out.println("Constructor is called");
+     }
+ 
+     public static void main(String[] args) {
+         cons obj = new cons();
+     } 
+ }
